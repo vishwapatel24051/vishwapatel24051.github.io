b1a0e2280bd6c4bba83e1c5e0dd8f962aa4a40d0
vishwapatel24051 <vishwapatel2405.1@gmail.com>
2026-07-22 13:20:38 -0700

    Add files via upload
    commit changes

---
+++ app.js
@@ -0,0 +1,13 @@
+(function(){
+  var els = document.querySelectorAll('.rv');
+  if(!('IntersectionObserver' in window)){
+    els.forEach(function(e){e.classList.add('in')});
+    return;
+  }
+  var io = new IntersectionObserver(function(entries){
+    entries.forEach(function(en){
+      if(en.isIntersecting){ en.target.classList.add('in'); io.unobserve(en.target); }
+    });
+  },{threshold:.12, rootMargin:'0px 0px -40px 0px'});
+  els.forEach(function(e){io.observe(e)});
+})();
