fbb8c414585946d78bb7778a495920f560813bfa
vishwapatel24051 <vishwapatel2405.1@gmail.com>
2026-07-22 15:08:26 -0700

    Add files via upload
    Redesign portfolio

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-(function(){
+(function () {
   var els = document.querySelectorAll('.rv');
-  if(!('IntersectionObserver' in window)){
-    els.forEach(function(e){e.classList.add('in')});
+
+  if (!('IntersectionObserver' in window)) {
+    els.forEach(function (e) { e.classList.add('in'); });
     return;
   }
-  var io = new IntersectionObserver(function(entries){
-    entries.forEach(function(en){
-      if(en.isIntersecting){ en.target.classList.add('in'); io.unobserve(en.target); }
+
+  var io = new IntersectionObserver(function (entries) {
+    entries.forEach(function (en) {
+      if (en.isIntersecting) {
+        en.target.classList.add('in');
+        io.unobserve(en.target);
+      }
     });
-  },{threshold:.12, rootMargin:'0px 0px -40px 0px'});
-  els.forEach(function(e){io.observe(e)});
+  }, { threshold: 0.12, rootMargin: '0px 0px -40px 0px' });
+
+  els.forEach(function (e) { io.observe(e); });
 })();
